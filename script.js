22e6e08f04d053e21c0825f8bf523bbb29aa832d
Willian Amancio <wvsamancio@gmail.com>
2022-06-26 10:28:06 -0300

    added clipboard action in fields: username and password

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ const searchInput = document.querySelector("[data-search]")
 
 let data = []
 
-searchInput.addEventListener("input", (e) => {
-    const value = e.target.value.toLowerCase()
+searchInput.addEventListener("input", (event) => {
+    const value = event.target.value.toLowerCase()
     data.forEach(each => {
         const isVisible = each.title.toLowerCase().includes(value) ||
             each.url.toLowerCase().includes(value) ||
@@ -17,15 +17,25 @@ searchInput.addEventListener("input", (e) => {
 })
 
 data = database.map(each => {
-    const card = dataCardTemplate.content.cloneNode(true).children[0]
+    const card = dataCardTemplate.content.cloneNode(true)
     const title = card.querySelector("[data-title]")
     const url = card.querySelector("[data-url]")
     const username = card.querySelector("[data-username]")
     const password = card.querySelector("[data-password]")
+    const copyUsername = card.querySelector("[copy-username]")
+    const copyPassword = card.querySelector("[copy-password]")
     title.textContent = each.title
     url.textContent = each.url
     username.textContent = each.username
     password.textContent = each.password
+    copyUsername.addEventListener("click", async (event) => {
+        await navigator.clipboard.writeText(username.textContent)
+        copyUsername.classList.add("success")
+    })
+    copyPassword.addEventListener("click", async (event) => {
+        await navigator.clipboard.writeText(password.textContent)
+        copyPassword.classList.add("success")
+    })
     dataPassCardTemplate.append(card)
     return { title: each.title, url: each.url, username: each.username, password: each.password, element: card }
 })
